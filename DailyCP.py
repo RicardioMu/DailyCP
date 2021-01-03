b2ac9cac20e8eb13c828c1bf48b266061d6c8783
RicardioMu <36025506+RicardioMu@users.noreply.github.com>
2021-01-04 02:59:30 +0800

    Update DailyCP.py

--- DailyCP.py
+++ DailyCP.py
@@ -14,7 +14,7 @@ from Crypto.Cipher import AES
 
 
 class DailyCP:
-    def __init__(self, schoolName="安徽理工大学"):
+    def __init__(self, schoolName="北部湾大学"):
         self.key = "ST83=@XV"  # dynamic when app update
         self.session = requests.session()
         self.host = ""
